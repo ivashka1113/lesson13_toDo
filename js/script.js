f44ad7e5216e837ea9f53412d8951668d40ec85e
ivashka1113 <102432109+ivashka1113@users.noreply.github.com>
2022-10-18 06:59:06 +0700

    Внёс правки

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ const toDoData = JSON.parse(localStorage.getItem('toDoData')) ? JSON.parse(local
 const render = function () {
     todoList.innerHTML = "";
     todoCompleted.innerHTML = "";
-    toDoData.forEach(function (item) {
+    toDoData.forEach(function (item, index) {
         const li = document.createElement('li');
         li.classList.add("todo-item");
         li.innerHTML = `<span class="text-todo">${item.text}</span>
@@ -26,7 +26,7 @@ const render = function () {
         };
 
         li.querySelector(".todo-remove").addEventListener("click", function () {
-            toDoData.splice(li, 1);
+            toDoData.splice(index, 1);
             li.remove();
             localStorage.setItem("toDoData", JSON.stringify(toDoData));
         })
